1183a4ec65c675c1e82775baff49886ab300a537
EstevaoMGomes <estevao.gomes04@gmail.com>
2024-03-12 11:22:53 +0000

    implementing jax for-loop

--- src/CreateCoil.py
+++ src/CreateCoil.py
@@ -2,6 +2,7 @@ from simsopt.geo import CurveXYZFourier
 from jax import jit
 from functools import partial
 import jax.numpy as jnp
+from jax.lax import fori_loop
 
 
 def oldCreateCoil(FourierCoefficients: list[int | float], NumberOfPoints: int, order: float) -> CurveXYZFourier:
@@ -27,6 +28,13 @@ def CreateCoil(dofs: jnp.ndarray, numquadpoints: int, order: int) -> jnp.ndarray
     quadpoints = jnp.linspace(0, 1, numquadpoints + 1)[:-1]
 
     data = jnp.outer(dofs[:, 0], jnp.ones(numquadpoints))
-    for j in range(1, order + 1):
-        data = data + jnp.outer(dofs[:, 2 * j - 1], jnp.sin(2 * jnp.pi * j * quadpoints)) + jnp.outer(dofs[:, 2 * j], jnp.cos(2 * jnp.pi * j * quadpoints))
+    
+    @jit
+    def fori_createdata(order_index: jnp.int32, data: jnp.ndarray) -> jnp.ndarray:
+        return data + jnp.outer(dofs[:, 2 * order_index - 1], jnp.sin(2 * jnp.pi * order_index * quadpoints)) + jnp.outer(dofs[:, 2 * order_index], jnp.cos(2 * jnp.pi * order_index * quadpoints))
+    data = fori_loop(1, order+1, fori_createdata, data) 
+    
+    
+    #for j in range(1, order + 1):
+    #    data = data + jnp.outer(dofs[:, 2 * j - 1], jnp.sin(2 * jnp.pi * j * quadpoints)) + jnp.outer(dofs[:, 2 * j], jnp.cos(2 * jnp.pi * j * quadpoints))
     return jnp.transpose(data)
